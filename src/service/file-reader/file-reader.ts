e960484b0f1a3fa8cef42a52a37f98ae79cf1071
Евгений Войтенко <jozik.service@gmail.com>
2021-02-02 17:42:41 +0300

    fix node types

--- src/service/file-reader/file-reader.ts
+++ src/service/file-reader/file-reader.ts
@@ -1,9 +1,9 @@
 import { IFileReaderService } from "./file-reader.interfaces";
 import { inject, injectable } from "inversify";
-import * as path from "path";
-import * as fs from "fs";
 import { TYPES } from "../../di/di-types";
 import { ILogger } from "../logger/logger.interface";
+import * as fs from "fs";
+import * as path from "path";
 
 @injectable()
 export class FileReader implements IFileReaderService {
