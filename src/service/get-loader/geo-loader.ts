55c2794121ef2a97682db1f5593433905717f26f
Евгений Войтенко <jozik.service@gmail.com>
2021-02-02 23:15:58 +0300

    add get gsons

--- src/service/get-loader/geo-loader.ts
+++ src/service/get-loader/geo-loader.ts
@@ -9,7 +9,8 @@ export class GeoLoaderService implements IGeoLoaderService {
   constructor(@inject(TYPES.Logger) private readonly logger: ILogger) {}
 
   public async loadGsons(cities: string[]): Promise<void> {
-    const osm: number[] = await this.getOsms(cities);
+    const osms: number[] = await this.getOsms(cities);
+    const gsons = await this.getGsons(osms);
   }
 
   private async getOsms(cities: string[]): Promise<number[]> {
@@ -32,6 +33,31 @@ export class GeoLoaderService implements IGeoLoaderService {
         }
       } catch (e) {
         this.logger.error(e, "Cant get OSM for:", city);
+        return [];
+      }
+    }
+    this.logger.log("Get total ", result.length, "osms");
+    return result;
+  }
+
+  private async getGsons(osms: number[]): Promise<Object[]> {
+    const result: Object[] = [];
+    for (const osm of osms) {
+      try {
+        this.logger.log("Get GeoJson for:", osm);
+        const url = encodeURI(` http:polygons.openstreetmap.fr/?id=${osm}`);
+        const res = await axios({
+          method: "get",
+          url: url,
+          responseType: "json",
+        });
+        const gson = res.data[0].osm_id;
+        if (osm) {
+          this.logger.log("Got GeoJson for", osm);
+          result.push(osm);
+        }
+      } catch (e) {
+        this.logger.error(e, "Cant get GeoJson for:", osm);
         return [];
       }
     }
